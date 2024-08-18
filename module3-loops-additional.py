ef12cdedc63b66130ecb4aebc116efd6b0d722b7
Fabby80 <fabian.chi@outlook.com>
2024-08-18 21:27:37 +0300

    commit

--- module3-loops-additional.py
+++ module3-loops-additional.py
@@ -1,10 +1,4 @@
-answer_a = int(input('Try to guess the first number: '))
- 
-if answer_a == 8:
-    answer_b = int(input('Correct! Now, guess the second number: '))
-    if answer_b == 5:
-        print('You won!')
-    else:
-        print('You only got one number right. So close!')
-else:
-    print('Wrong!')
+for a in range(1, 6):
+    for b in range(1, 10):
+         if (a == b):
+           print(a)
